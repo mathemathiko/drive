7803ef1b6313787b7269210c9aac81985a5d492d
mathiko <my.tears13@gmail.com>
2014-03-09 16:44:36 +0900

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Drive::Application.config.secret_key_base = '72768f0d39adc8a0376fb523de3c72522fea3e090b662c86367d99f37d7b3caf062b7895a7719ea65569918cecd74f7778c4db43e66938c23294f0884409d385'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Drive::Application.config.session_store :cookie_store, key: '_drive_session'
