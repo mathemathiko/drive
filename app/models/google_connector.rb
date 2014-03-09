812933cdc4f2c83e7b4cf310b4ebbb308ef7ce44
mathiko <my.tears13@gmail.com>
2014-03-09 17:17:32 +0900

    Add basic logic

--- app/models/google_connector.rb
+++ app/models/google_connector.rb
@@ -1,2 +1,30 @@
+require 'logger'
+
+# class GoogleDriveConnector
 class GoogleConnector
+  API_VERSION = 'v2'
+
+  attr_accessor :client
+
+  def initialize
+    @client = initialize_client
+    prepare_log
+  end
+
+  private
+
+  def initialize_client
+    Google::APIClient.new(
+      :application_name    => 'GoogleConnector',
+      :application_version => '1.0.0'
+    )
+  end
+
+  def prepare_log
+    log_file = File.open 'drive.log', 'a+'
+    log_file.sync = true
+
+    logger = Logger.new(log_file)
+    logger.level = Logger::DEBUG
+  end
 end
--- spec/models/google_connector_spec.rb
+++ spec/models/google_connector_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe GoogleConnector do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "initialize" do
+    before do
+      @connector = GoogleConnector.new
+    end
+
+    context "normal case" do
+      it "initialize_client" do
+        @connector.instance_variable_get(:@client).class.should Google::APIClient
+      end
+    end
+  end
 end
